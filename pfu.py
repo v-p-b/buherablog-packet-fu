6ed0889a73259a38a8caff563f91ce04ee2f720b
2501 <bh.gitorious@freemail.hu>
2013-03-06 16:43:46 +0100

    gwscan updated, now u can run scans on net/mask too

--- pfu.py
+++ pfu.py
@@ -97,36 +97,34 @@ class gwscan(GetMacsMixin):
 
 	def usage(self):
 		print "Usage:"
-		print "\t%s gwscan <target_net> <target_ip>" % sys.argv[0]
-		print "example:"
+		print "\t%s gwscan <local_subnet> <target_ip/net>" % sys.argv[0]
+		print "examples:"
 		print "\t%s gwscan 192.168.1.0/24 8.8.8.8" % sys.argv[0]
+		print "\t%s gwscan 192.168.1.0/24 10.0.0.0/24" % sys.argv[0]
 
 	def start(self):
-		ret = self.gwscan_broadcast(self.net, self.ip)
+		ret = self.gwscan_icmp(self.net, self.ip)
 		for x in ret:
-			print "%18s %16s" % (x['gw_mac'], x['gw_ip'])
+			print "%18s %16s %16s" % (x['gw_mac'], x['gw_ip'], x['r_ip'])
 
-	def gwscan_broadcast(self, net, ip):
+	def gwscan_icmp(self, net, ip):
 		log.msg('gwscan for net %s, searching gw for %s' %(net, ip))
 		lt = self.getmacs(net)
-#		ans,unans = scapy.srp(scapy.Ether(dst='ff:ff:ff:ff:ff:ff') / scapy.IP(dst=ip) / scapy.TCP(sport=11111, dport=80, flags="S"), timeout=1)
-#		ans,unans = scapy.srp(scapy.Ether(dst='ff:ff:ff:ff:ff:ff') / scapy.IP(dst=ip) / scapy.ICMP(), timeout=2)
-		ans,unans = scapy.srp(scapy.Ether(dst=lt['mac_ip'].keys()) / scapy.IP(dst=ip) / scapy.ICMP(), timeout=2)
-#		ans.show()
+		#ans,unans = scapy.srp(scapy.Ether(dst='ff:ff:ff:ff:ff:ff') / scapy.IP(dst=ip) / scapy.ICMP(), timeout=5)
+		ans,unans = scapy.srp(scapy.Ether(dst=lt['mac_ip'].keys()) / scapy.IP(dst=ip) / scapy.ICMP(), timeout=5)
 		ret = []
-		for a in ans:
-#			a[1].show()
-			if a[1][scapy.ICMP].type == 0 and a[1][scapy.ICMP].code == 0:
-				mac = a[1][scapy.Ether].src
-				r_ip = a[1][scapy.IP].src
-				ip = lt['mac_ip'][mac]
-				ret.append({
-					'ttype':	'ping',
-					'gw_mac':	mac,
-					'gw_ip':	ip,
-#					'r_ip':		r_ip
-#					't_ip':		t_ip
-				})
+		for b in ans:
+			for a in b[1]:
+				if a[scapy.ICMP].type == 0 and a[scapy.ICMP].code == 0:
+					mac = a[scapy.Ether].src
+					r_ip = a[scapy.IP].src
+					ip = lt['mac_ip'][mac]
+					ret.append({
+						'ttype':	'ping',
+						'gw_mac':	mac,
+						'gw_ip':	ip,
+						'r_ip':		r_ip
+					})
 		log.msg('gwscan finished')
 		return ret
 
